6bc66531affe3e23505471956c9d627bfa21e0c3
Calvin Park <nofxincubus@gmail.com>
2012-03-10 00:12:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edacioserver::Application.config.secret_token = '28fd52f163f77114873e1ae158c34c57a600e134db332c6fa864431c4f9d53659838894f3d582a450c3214439ecd146ef17a14f50b03be280f59bbb890586faa'
