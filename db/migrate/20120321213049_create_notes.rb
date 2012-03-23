f299e30b295309348316d5862468524b016cde66
Calvin Park <nofxincubus@gmail.com>
2012-03-23 14:27:31 -0400

    fixed user_id variable

--- db/migrate/20120321213049_create_notes.rb
+++ db/migrate/20120321213049_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
       t.string :content
-		t.string :user_id
+		t.integer :user_id
       t.integer :connection_id
 
       t.timestamps
